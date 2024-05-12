31614d02f03643b1ed5c748282364712ab63f64f
Bicky Tamang <bickytamang101@gmail.com>
2024-05-12 23:23:55 +0545

    formatting of the color in the tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,9 @@ export default {
       boxShadow: {
         primary: "0 4px 30px rgba(0, 0, 0, 0.1)",
         "bevel-primary":
-          "inset 0.8rem 0.8rem 1rem 0 rgba(234,139,139,1), inset -1.25rem -1.25rem 1rem 0 rgba(201,90,123,1)",
+          "inset 0.8rem 0.8rem 1rem 0 rgba(234, 139, 139, 1), inset -1.25rem -1.25rem 1rem 0 rgba(201, 90, 123, 1)",
         "bevel-secondary":
-          "inset 0.8rem 0.8rem 1rem 0 rgba(241,148,130,1), inset -1.25rem -1.25rem 1rem 0 rgba(216,109,117,1)",
+          "inset 0.8rem 0.8rem 1rem 0 rgba(241, 148, 130, 1), inset -1.25rem -1.25rem 1rem 0 rgba(216, 109, 117, 1)",
       },
       width: {
         70: "70px",
